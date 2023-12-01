5a2baa974e1ed186c777ab30df9f46a958ae89d3
akashkr2331 <84612937+akashkr2331@users.noreply.github.com>
2023-12-01 18:24:17 +0530

    Update index.php

--- admin page/index.php
+++ admin page/index.php
@@ -1,17 +1,17 @@
-
 <?php
-require_once "connection.php";
-
-if(count($_POST)>0) {
-    mysqli_query($conn,"UPDATE users set  name='" . $_POST['name'] . "', phone='" . $_POST['phone'] . "' ,email='" . $_POST['email'] . "', 
+// require_once "connection.php";
+include_once 'database.php';
+if(isset($_POST['name'])) {
+    mysqli_query($conn,"UPDATE user set  name='" . $_POST['name'] . "', phone='" . $_POST['phone'] . "' ,email='" . $_POST['email'] . "', 
     Hatchback='" . $_POST['Hatchback'] . "' , SUV='" . $_POST['SUV'] . "' , Sadan='" . $_POST['Sadan'] . "' 
-     WHERE id='" . $_POST['id'] . "'");
+     WHERE sno='" . $_POST['sno'] . "'");
     $message = "Record Modified Successfully";
-}
 
-$result = mysqli_query($conn,"SELECT * FROM users WHERE id='" . $_GET['userid'] . "'");
-Ezoic
+
+$result = mysqli_query($conn,"SELECT * FROM user WHERE sno='" . $_POST['sno'] . "'");
+
 $row= mysqli_fetch_array($result);
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +20,7 @@ $row= mysqli_fetch_array($result);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin page</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
@@ -27,51 +28,51 @@ $row= mysqli_fetch_array($result);
     Admin Page
 </h1>
 
+<div class="banner">
+                        <img src="images/front-left-side-47 (8).webp" alt="" width=100% height=600px>
+                    </div>
+
 <h2>
     Databases
-</h2>
+</h2> 
+
 
 <?php
 
-$servername='localhost';
-$username='root';
-$password='';
-$dbname = "my_db";
-
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-  
-if(!$conn){
-    die('Could not Connect MySql Server:' .mysql_error());
-}
-
-?>
-
-<?php
-                        include_once 'db.php';
-                        $result = mysqli_query($conn,"SELECT * FROM users");
+                        include_once 'database.php';
+                        $result = mysqli_query($conn,"SELECT * FROM user");
+                        
                     ?>
 
 <?php
+// foreach($row in $result){
+    // echo $result["0"]["name"];
                         if (mysqli_num_rows($result) > 0) {
                     ?>
 
-<form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+<?php
+                            $i=0;
+                            while($row = mysqli_fetch_array($result)) {
+                        ?>
+
+<form action="index.php" method="post">
                         <div class="form-group">
                             <label>Name</label>
                             <input type="text" name="name" class="form-control" value="<?php echo $row["name"]; ?>">
+                            
                         </div>
-                        <div class="form-group ">
+                        <div class="form-group">
                             <label>Email</label>
                             <input type="email" name="email" class="form-control" value="<?php echo $row["email"]; ?>">
                         </div>
                         <div class="form-group">
                             <label>Phone</label>
-                            <input type="phone" name="phone" class="form-control" value="<?php echo $row["phone"]; ?>">
+                            <input type="text" name="phone" class="form-control" value="<?php echo $row["phone"]; ?>">
                         </div>
 
                         <div class="form-group">
                             <label>Address</label>
-                            <input type="Address" name="Address" class="form-control" value="<?php echo $row["Address"]; ?>">
+                            <input type="Address" name="Address" class="form-control" value="<?php echo $row["address"]; ?>">
                         </div>
 
                         <div class="form-group">
@@ -89,35 +90,46 @@ if(!$conn){
                             <input type="Sadan" name="Sadan" class="form-control" value="<?php echo $row["Sadan"]; ?>">
                         </div>
 
-                        <input type="hidden" name="id" value="<?php echo $row["id"]; ?>"/>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="users.php" class="btn btn-default">Cancel</a>
+                        <input type="hidden" name="sno" value="<?php echo $row["sno"]; ?>"/>
+                        <!-- <button type="submit" class="btn btn-primary" value="Submit"> -->
+                        <button type="submit" id="submit-btn" > Submit</button>
+                        <!-- <a href="users.php" class="btn btn-default">Cancel</a> -->
+                        <a href="delete-process.php?sno=<?php echo $row["sno"]; ?>">Delete</a>
                     </form>
 
-
+                    <?php
+                            $i++;
+                            }
+                        ?>
                      <?php
                         }else{
                             echo "No result found";
                         }
                     ?>
 
+                    
+
 <h2>
     Most Searched Cars
 </h2>
 
 <div class="com">
-    <span>
-        <img src="images/front-left-side-47" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (1)" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (2)" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (3)" alt="">
-    </span>
+    <div class="search">
+        <img src="images/front-left-side-47.webp" alt="">
+        <div>Mahindra Thar</div>
+    </div>
+    <div class="search">
+        <img src="images/front-left-side-47 (1).webp" alt="">
+        <div>Tata Nexon</div>
+    </div>
+    <div class="search">
+        <img src="images/front-left-side-47 (2).webp" alt="">
+        <div>Tata Punch</div>
+    </div>
+    <div class="search">
+        <img src="images/front-left-side-47 (3).webp" alt="">
+        <div>Maruti Brezza</div>
+    </div>
 
 </div>
 
@@ -126,24 +138,35 @@ if(!$conn){
 </h2>
 
 <div class="com">
-<span>
-        <img src="images/front-left-side-47 (7)" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (6)" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (5)" alt="">
-    </span>
-    <span>
-        <img src="images/front-left-side-47 (4)" alt="">
-    </span>
+<div class="latest">
+        <img src="images/front-left-side-47 (4).webp" alt="">
+        <div>Lexus RX</div>
+    </div>
+    <div class="latest">
+        <img src="images/front-left-side-47 (5).webp" alt="">
+        <div>Mercedes-Benz A-Class Limousine</div>
+    </div>
+    <div class="latest">
+        <img src="images/front-left-side-47 (6).webp" alt="">
+        <div>BMW X7</div>
+    </div>
+    <div class="latest">
+        <img src="images/front-left-side-47 (7).webp" alt="">
+        <div>Audi Q3</div>
+    </div>
 
 </div>
 
 <footer>
     <p>© 2023 Unbundl Assignment</p>
   </footer>
+
+  <!-- <script>
+    const delete=document.getElementsbyClassname("delete");
+    delete.addEventListener('click',()=>{
+
+    })
+  </script> -->
 
 </body>
 </html>
